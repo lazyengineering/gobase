8d3cd09ec47a90ee9a9c0c1c5c08d5a284df5f6b
Jesse Allen <jesse@jessecarl.com>
2013-11-18 14:59:22 -0700

    Documentation for layouts package

--- layouts/layout.go
+++ layouts/layout.go
@@ -12,26 +12,36 @@ import (
 	"time"
 )
 
+// Layout defines a collection of templates we can use throughout a site,
+// including the "default" template that we execute.
 type Layout struct {
 	patterns     []string
 	functions    template.FuncMap
 	baseTemplate string
 }
 
+// Create a new Layout from the provided function map, base template name, and set of
+// Globs where template files can be located.
 func New(functions template.FuncMap, baseTemplate string, patterns ...string) *Layout {
 	l := new(Layout)
 	l.Init(functions, baseTemplate, patterns...)
 	return l
 }
 
+// Initialize a layout with the provided function map, base template name, and set of
+// Globs where template files can be located.
 func (l *Layout) Init(functions template.FuncMap, baseTemplate string, patterns ...string) {
 	l.functions = functions
 	l.baseTemplate = baseTemplate
 	l.patterns = patterns
 }
 
+// An Action does the unique work for an http response where the result should be
+// a page rendered from a template executed with unique data.
 type Action func(*http.Request) (map[string]interface{}, error)
 
+// Returns an Action that runs the original Action when there is no cached value.
+// The cached value is unset after the given ttl (time to live) duration.
 func (a Action) Cache(ttl time.Duration) Action {
 	var data map[string]interface{}
 	lock := sync.RWMutex{}
@@ -58,8 +68,11 @@ func (a Action) Cache(ttl time.Duration) Action {
 	}
 }
 
+// The signature for a function that will be used when an error occurs with an Action
 type ErrorHandler func(http.ResponseWriter, *http.Request, error)
 
+// Use Act in order to create an http.Handler that fills a template with the data from an executed Action
+// or executes the ErrorHandler in case of an error.
 func (l *Layout) Act(respond Action, eh ErrorHandler, templates ...string) http.Handler {
 	// Load templates so that we can clone instead of loading every time
 	permanentTemplates := template.Must(l.load(templates...))
@@ -87,7 +100,6 @@ func (l *Layout) Act(respond Action, eh ErrorHandler, templates ...string) http.
 	})
 }
 
-// TODO: if performance becomes an issue, we can start caching the base templates, and cloning
 func (l *Layout) load(patterns ...string) (*template.Template, error) {
 	var err error
 	// add some key helper functions to the templates
@@ -101,6 +113,10 @@ func (l *Layout) load(patterns ...string) (*template.Template, error) {
 	return b, nil
 }
 
+// BasicFunctionMap returns a simple template.FuncMap containing the following helper functions
+//
+//     markdownCommon(raw string) template.HTML // process markdown formatted string using github.com/russross/blackfriday MarkdownCommon
+//     markdownBasic(raw string)  template.HTML // process markdown formatted string using github.com/russross/blackfriday MarkdownBasic
 func BasicFunctionMap() template.FuncMap {
 	return template.FuncMap{
 		"markdownCommon": func(raw string) template.HTML {
