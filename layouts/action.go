62c6601f28bc162869f87ccdb434190f4b2d9326
Jesse Allen <jesse.carl@gmail.com>
2014-10-01 19:28:42 -0700

    Add Merge function for Actions
    Adds Merge, which allows multiple actions to merge into a single
    action. This is useful for adding uniform navigation, shared elements,
    etc to sites.

--- layouts/action.go
+++ layouts/action.go
@@ -4,6 +4,7 @@
 package layouts
 
 import (
+	"errors"
 	"net/http"
 	"sync"
 	"time"
@@ -42,4 +43,81 @@ func (a Action) cache(ttl time.Duration) Action {
 		}
 		return data, err
 	}
+}
+
+// Calls several actions together in order to return a superset of data.
+// Duplicate keys in the returned map are resolved with the first action
+// in the arguments taking priority, except where the value is a slice of
+// strings, which will be merged.
+func Merge(actions ...Action) Action {
+	return func(req *http.Request) (map[string]interface{}, error) {
+		done := make(chan struct{})
+		defer close(done)
+
+		c, errc := mergeActions(done, actions, req)
+
+		unmergedData := make([]map[string]interface{}, len(actions), len(actions))
+		for r := range c {
+			if r.Err != nil {
+				return nil, r.Err
+			}
+			unmergedData[r.Index] = r.Data
+		}
+		if err := <-errc; err != nil {
+			return nil, err
+		}
+		mergedData := make(map[string]interface{})
+		for _, d := range unmergedData {
+			for key, val := range d {
+				if mergedData[key] == nil {
+					mergedData[key] = val
+				} else if v1, isStringSlice := mergedData[key].([]string); isStringSlice {
+					if v2, bothStringSlices := val.([]string); bothStringSlices {
+						mergedData[key] = append(v1, v2...)
+					}
+				}
+			}
+		}
+		return mergedData, nil
+	}
+}
+
+type actionResult struct {
+	Index int
+	Data  map[string]interface{} // result data
+	Err   error
+}
+
+func mergeActions(done <-chan struct{}, actions []Action, req *http.Request) (<-chan actionResult, <-chan error) {
+	c := make(chan actionResult)
+	errc := make(chan error, 1)
+	go func() {
+		var wg sync.WaitGroup
+		var canceled error
+		// loop over seed data
+		for i, a := range actions {
+			wg.Add(1)
+			go func(idx int, action Action) {
+				// do the work
+				data, err := action(req)
+				select {
+				case c <- actionResult{idx, data, err}:
+				case <-done:
+				}
+				wg.Done()
+			}(i, a)
+			select {
+			case <-done:
+				canceled = errors.New("actions canceled")
+			default:
+				canceled = nil
+			}
+		}
+		go func() {
+			wg.Wait()
+			close(c)
+		}()
+		errc <- canceled
+	}()
+	return c, errc
 }
